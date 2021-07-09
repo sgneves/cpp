da783d8cb5d02aefd86bed7f0f6c403bdc43f616
Sergio Neves <sgneves@gmail.com>
2021-07-09 17:05:10 +0100

    Current working directory
    Current working directory changed to batch file location before executing the rest of the code

--- C++_Primer/Exercise_01_01/Run.bat
+++ C++_Primer/Exercise_01_01/Run.bat
@@ -1,6 +1,12 @@
 
 @echo off
 
+set folder=%cd%
+
+cd %~dp0
+
 .\Debug\Exercise_01_01.exe
 
 echo %ERRORLEVEL%
+
+cd %folder%
--- C++_Primer/Exercise_01_02/Run.bat
+++ C++_Primer/Exercise_01_02/Run.bat
@@ -1,6 +1,12 @@
 
 @echo off
 
+set folder=%cd%
+
+cd %~dp0
+
 .\Debug\Exercise_01_02.exe
 
 echo %ERRORLEVEL%
+
+cd %folder%
